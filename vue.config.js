16f58fef05768e6f6cc3a39d09783f5dd2199304
houfan <364889408@qq.com>
2020-11-03 13:52:10 +0800

    代码提交

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+// module.exports = {
+//     devServer: {
+//         // open: true,
+//         host: 'localhost',
+//         port: 8080,
+//         // contentBase: path.join(__dirname, 'public'),
+//         https: false,
+//         hotOnly: false,
+//         proxy: {
+//             // 配置跨域
+//             "/api": {
+//                 target: 'http://localhost:8080/',
+//                 // changeOrigin: true,
+//                 pathRewrite: {
+//                     '^/api': '/static/aaa.json'
+//                 }
+//             }
+//         }
+//     }
+// }
